6cedce6c0101b4c284807c9080a47bc38a3dda67
Alberto Romeu <aromeu@prodevelop.es>
2016-08-18 12:46:09 +0200

    Vagrantfile for ELK stack ready to monitor the virtual machine wqwith topbeat

---
+++ ELK/README.md
@@ -0,0 +1,9 @@
+A virtual machine with the ELK Stack
+
+- ElasticSearch
+- Logstash
+- Kibana
+- FileBeat
+- topbeat
+
+Once it starts go to: http://localhost:5601 configure the topbeat index and load the topbeat dashboard
---
+++ ELK/bootstrap.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+# https://www.elastic.co/guide/en/beats/libbeat/current/beats-reference.html
+
+# update and install required packages
+sudo apt-get update -y
+sudo apt-get install unzip openjdk-7-jre -y
+
+INSTALL_DIR=/opt/elk
+mkdir -p $INSTALL_DIR
+
+# install ElasticSearch
+# https://www.elastic.co/guide/en/beats/libbeat/current/elasticsearch-installation.html
+cd $INSTALL_DIR
+curl -L -O https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-2.3.3.deb
+sudo dpkg -i elasticsearch-2.3.3.deb
+sudo /etc/init.d/elasticsearch start &
+
+# install logstash and filebeat plugin
+# https://www.elastic.co/guide/en/beats/libbeat/current/logstash-installation.html
+cd $INSTALL_DIR
+curl -L -O https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.3.2-1_all.deb
+sudo dpkg -i logstash_2.3.2-1_all.deb
+cd /etc/logstash
+./bin/logstash-plugin install logstash-input-beats
+
+sudo cat <<EOF > /etc/logstash/conf.d/logstash.conf
+input {
+  beats {
+    port => 5044
+  }
+}
+
+output {
+  elasticsearch {
+    hosts => "localhost:9200"
+    manage_template => false
+    index => "%{[@metadata][beat]}-%{+YYYY.MM.dd}"
+    document_type => "%{[@metadata][type]}"
+  }
+}
+EOF
+
+sudo /etc/init.d/logstash start &
+
+# install Kibana
+# https://www.elastic.co/guide/en/beats/libbeat/current/kibana-installation.html
+cd $INSTALL_DIR
+#curl -L -O https://download.elastic.co/kibana/kibana/kibana-4.5.1-linux-x64.tar.gz
+#tar xzvf kibana-4.5.1-linux-x64.tar.gz
+#cd kibana-4.5.1-linux-x64/bin
+#sudo ./kibana &
+echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
+sudo apt-get update -y
+sudo apt-get -y install kibana
+sudo update-rc.d kibana defaults 96 9
+sudo service kibana start
+
+
+# loading the beats dashboards
+# https://www.elastic.co/guide/en/beats/libbeat/current/load-kibana-dashboards.html
+cd $INSTALL_DIR
+curl -L -O http://download.elastic.co/beats/dashboards/beats-dashboards-1.2.3.zip
+unzip beats-dashboards-1.2.3.zip
+cd beats-dashboards-1.2.3/
+./load.sh
+
+# install beats
+# https://www.elastic.co/guide/en/beats/libbeat/current/installing-beats.html
+cd $INSTALL_DIR
+curl https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://packages.elastic.co/beats/apt stable main" |  sudo tee -a /etc/apt/sources.list.d/beats.list
+sudo apt-get update && sudo apt-get install filebeat
+
+# install top beat
+# https://www.elastic.co/guide/en/beats/topbeat/1.2/topbeat-getting-started.html
+cd $INSTALL_DIR
+curl -L -O https://download.elastic.co/beats/topbeat/topbeat_1.2.3_amd64.deb
+sudo dpkg -i topbeat_1.2.3_amd64.deb
+sudo cat <<EOF > /etc/topbeat/topbeat.yaml
+input:
+  period: 10
+  procs: [".*"]
+output:
+  logstash:
+    hosts: ["127.0.0.1:5044"]
+
+    # Optional load balance the events between the Logstash hosts
+    #loadbalance: true
+EOF
+sudo /etc/init.d/topbeat start &
+
+
